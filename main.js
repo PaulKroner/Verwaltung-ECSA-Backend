6635d3cbcb4b944d6e29de06aead4dfbab85b4dd
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-08-01 08:38:51 +0200

    cleaned main.js

--- main.js
+++ main.js
@@ -4,43 +4,27 @@ const db = require('./db');
 const app = express();
 const port = 8080;
 
-const overviewRoute = require("./routes/overview");
-const testdbRoute = require("./routes/testDB");
-
-app.use(cors());  // Use the cors middleware
-
-// Middleware to log all incoming requests
-app.use((req, res, next) => {
-    console.log(`Received request for ${req.path}`);
-    next();
-});
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    console.log("Handling route /");
-    res.json({ message: "Route /" });
-    console.log("Response sent for route /");
+    // console.log("Handling route /");
+    res.json({ message: "main.js page" });
+    // console.log("Response sent for route /");
 });
 
-app.get("/api/test", (req, res) => {
-    console.log("Handling route /api/test");
-    res.json({ message: "Hello, World!", people: ["Harry", "Paul", "Peter"] });
-    console.log("Response sent for route /api/test");
+// Route für die SQL-Abfrage
+app.get('/api/data', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM pqtest');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Interner Serverfehler' });
+    }
 });
 
-// Use routes
-app.use('/overview', overviewRoute);
-app.use('/testdb', testdbRoute);
-
-// app.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}`);
-// });
-
-db.query(`Select * from pgtest`, (err, res) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(res.rows);
-        console.log("2. mal")
-    }
+//   express seems like to have a problem with the lines below
+app.listen(port, () => {
+    console.log(`Server läuft auf http://localhost:${port}`);
 });
