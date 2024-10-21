fcf6ec1fe716c16a264a633ce6ccfa3ccfc2e176
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-10-21 09:18:45 +0200

    Passwort vergessen Meldung "E-Mail-Adresse nicht vorhanden" fehlt #25 fixed

--- sendPasswordResetEmail.js
+++ sendPasswordResetEmail.js
@@ -12,14 +12,7 @@ const pool = require('./db'); // Verbindung zur Datenbank
 const sendPasswordResetEmail = async (req, res) => {
   const { email } = req.body;
   try {
-
     const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-
-    if (result.rows.length === 0) {
-      return res.status(400).json({ message: 'Benutzer nicht gefunden.' });
-    }
-
-    const user = result.rows[0];
 
     // generate reset-token
     const token = crypto.randomBytes(32).toString('hex');
@@ -27,28 +20,33 @@ const sendPasswordResetEmail = async (req, res) => {
     // set expiry date to 15 minutes from now
     const expiryDate = new Date(Date.now() + 900000); // 15 min in milliseconds
 
-    // save token and expiry date to database
-    await pool.query(
-      `UPDATE users SET reset_token = $1, reset_token_expiry = $2 WHERE email = $3`,
-      [token, expiryDate, email]
-    );
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
 
-    const resetLink = `http://localhost:3000/registration/resetPassword/${token}`;
-    const message = `
-      <p>Klicke auf den folgenden Link, um dein Passwort zurückzusetzen:</p>
-      <a href="${resetLink}">${resetLink}</a>
-    `;
+      // save token and expiry date to database if user exists
+      await pool.query(
+        `UPDATE users SET reset_token = $1, reset_token_expiry = $2 WHERE email = $3`,
+        [token, expiryDate, email]
+      );
 
-    // send email
-    await sendEmail(email, 'Passwort zurücksetzen', message);
+      const resetLink = `http://localhost:3000/registration/resetPassword/${token}`;
+      const message = `
+        <p>Klicke auf den folgenden Link, um dein Passwort zurückzusetzen:</p>
+        <a href="${resetLink}">${resetLink}</a>
+      `;
 
-    res.json({ message: 'E-Mail zum Zurücksetzen des Passworts wurde gesendet.' });
+      // send email
+      await sendEmail(email, 'Passwort zurücksetzen', message);
+    }
+
+    // Gebe immer die gleiche Antwort zurück, unabhängig davon, ob die E-Mail existiert oder nicht
+    res.json({ message: 'Wenn ein Konto mit dieser E-Mail-Adresse existiert, wurde eine E-Mail zum Zurücksetzen des Passworts gesendet.' });
   } catch (error) {
     res.status(500).json({ message: 'Ein Fehler ist aufgetreten.' });
-    console.log(error.message)
+    console.log(error.message);
   }
 };
 
 module.exports = {
   sendPasswordResetEmail,
-}
+};
